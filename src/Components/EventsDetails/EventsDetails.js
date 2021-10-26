5c785bc859ca3f1a57131d5b5c3aa2a1018a3c4c
sharif764 <ss1884088@gmail.com>
2021-10-27 00:31:12 +0600

    J

---
+++ src/Components/EventsDetails/EventsDetails.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const EventsDetails = () => {
+    return (
+        <div>
+            <h1>Event Details</h1>
+        </div>
+    );
+};
+
+export default EventsDetails;
