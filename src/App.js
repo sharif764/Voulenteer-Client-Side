88aeb877b6fc740a5cd0be7ff14b3a5d37cdcd0b
sharif764 <ss1884088@gmail.com>
2021-10-27 00:39:39 +0600

    J

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Footer from './Components/Footer/Footer';
+import Header from './Components/Header/Header';
+import Home from './Components/Home/Home';
 
 function App() {
   return (
     <div className="App">
-
+      <Router>
+        <Header></Header>
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+        </Switch>
+        {/* <Footer></Footer> */}
+      </Router>
     </div>
   );
 }
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
     return (
         <div>
-            <h1>This is header</h1>
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/events">Events</NavLink>
+            <NavLink to="/donation">Donation</NavLink>
+            <NavLink to="blogs">Blogs</NavLink>
         </div>
     );
 };
